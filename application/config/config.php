b016823fc0a071fcc521ef1e83e097a51248ac2e
Arif Fadly Siregar <arif_siregar@biznetnetworks.com>
2020-08-05 04:42:03 +0700

    add Procfile

--- application/config/config.php
+++ application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = 'https://siaksman6.herokuapp.com';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'controllers/login/index';
 
 /*
 |--------------------------------------------------------------------------
@@ -381,7 +381,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
