1042cf85da02fca370999cc915238d5e82bdbf1e
Arif Fadly Siregar <arif_siregar@biznetnetworks.com>
2020-08-05 11:59:54 +0700

    rename controllers

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/siak2';
-// $config['base_url'] = 'https://siaksman6.herokuapp.com';
-$config['base_url'] = '';
+$config['base_url'] = 'https://siaksman6.herokuapp.com';
+// $config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -380,10 +380,22 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
+// $config['sess_driver'] = 'files';
+// $config['sess_cookie_name'] = 'ci_session';
+// $config['sess_expiration'] = 7200;
+// $config['sess_save_path'] = NULL;
+// $config['sess_match_ip'] = FALSE;
+// $config['sess_time_to_update'] = 300;
+// $config['sess_regenerate_destroy'] = FALSE;
+
+$sessDir = session_save_path();
+$sessDir = "{$sessDir}/sessionPath";
+is_dir($sessDir)?:mkdir($sessDir);
+
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = sys_get_temp_dir();
+$config['sess_save_path'] = $sessDir;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
