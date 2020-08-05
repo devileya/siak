b5f1a2348fd4670c01636051f31359c469da6362
Arif Fadly Siregar <arif_siregar@biznetnetworks.com>
2020-08-05 11:44:02 +0700

    rename controllers

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/siak2';
-// $config['base_url'] = 'https://siaksman6.herokuapp.com';
+// $config['base_url'] = 'http://localhost/siak2';
+$config['base_url'] = 'https://siaksman6.herokuapp.com';
 // $config['base_url'] = '';
 
 /*
