ff20854f89906cd4a9716c2f4ae6c81d64d36f40
Arif Fadly Siregar <arif_siregar@biznetnetworks.com>
2020-10-14 22:15:24 +0700

    fixed bugs

--- application/views/dashboard/footer.php
+++ application/views/dashboard/footer.php
@@ -1,12 +1,10 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="<?= base_url('asset_dashboard/assets/js/plugins.js') ?>"></script>
-<!-- <script src="<?= base_url('asset_dashboard/assets/js/main.js') ?>"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
 
-<!-- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> -->
+<script src="<?= base_url('asset_dashboard/assets/js/plugins.js') ?>"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.0/moment.min.js"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="<?= base_url('asset_dashboard/assets/js/bootstrap.bundle.min.js') ?>"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/js/tempusdominus-bootstrap-4.min.js" integrity="sha512-2JBCbWoMJPH+Uj7Wq5OLub8E5edWHlTM4ar/YJkZh3plwB2INhhOC3eDoqHm1Za/ZOSksrLlURLoyXVdfQXqwg==" crossorigin="anonymous"></script>
 
